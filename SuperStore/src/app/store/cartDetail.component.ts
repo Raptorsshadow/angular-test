82025ec13777721d14718e8da5f4e8c574424b02
Billy Larsen <raptorsshadow@gmail.com>
2018-11-03 03:12:20 -0400

    Added PWA Support.  Cleaned up Ship Capability.

--- SuperStore/src/app/store/cartDetail.component.ts
+++ SuperStore/src/app/store/cartDetail.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 import { Cart } from '../model/cart.model';
+import { ConnectionService } from '../model/connection.service';
 @Component({
 	templateUrl: 'cartDetail.component.html'
 })
 export class CartDetailComponent {
-	constructor(public cart: Cart) {}
+	public connected: boolean = false;
+	constructor(public cart: Cart, private connection: ConnectionService) {
+		this.connected = this.connection.connected;
+		connection.Changes.subscribe((state) => this.connected = state);
+		console.log(`Connection State has changed.  Connected state: ${this.connected}`);
+	}
 }
